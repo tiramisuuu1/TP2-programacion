f582635816e0abfafbc161b57878713a0c98e79a
tiramisu <polentita9@gmail.com>
2025-05-29 08:28:58 -0300

    Add files via upload

---
+++ actividad 1-2 TP2 programacion.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int num1, num2;
+   
+    cout<<"Ingresar dos numeros: "<<endl;
+	cout<<"Primer numero: ";
+			cin>>num1;
+	cout<<"Segundo numero: ";
+			cin>>num2;
+    if (num1 > num2)
+		{
+			cout<<"El numero "<<num1<<" es mayor al "<<num2;
+		}
+		else
+		{
+		    cout<<"El numero "<<num2<<" es mayor al "<<num1;
+		}
+			
+  
+    
+        
+         
+	return 0;
+}
+
+
